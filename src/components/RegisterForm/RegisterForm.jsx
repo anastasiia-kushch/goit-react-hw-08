df5ed7b5d6a08e6724d8fb48e7af40ca65abac1a
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-04-10 21:17:35 +0200

    -

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,24 +1,47 @@
 import { Field, Form, Formik } from 'formik';
 import { useDispatch } from 'react-redux';
-import css from '../RegisterForm/RegisterForm.module.css'
+import css from '../RegisterForm/RegisterForm.module.css';
+import { useId } from 'react';
+import { register } from '../../redux/auth/operations';
+import * as Yup from 'yup';
+
+const initialValues = {
+  name: '',
+  email: '',
+  password: '',
+};
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .trim()
+    .min(3, 'Too short!')
+    .max(50, 'Too long!')
+    .required('Required'),
+  password: Yup.string().trim().min(4, 'Too short!').required('Required'),
+  email: Yup.string().trim().email().required('Required'),
+});
 
 export default function RegisterForm() {
+  const nameFieldId = useId();
+  const emailFieldId = useId();
+  const passwordFieldId = useId();
   const dispatch = useDispatch();
 
   const handleSubmit = (values, actions) => {
     console.log(values);
-    dispatch();
+    dispatch(register(values));
     actions.resetForm();
   };
   return (
-    <Formik onSubmit={handleSubmit}>
+    <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={validationSchema}>
       <Form className={css.container}>
-        <label htmlFor="username">Username</label>
-        <Field name="username"></Field>
-        <label htmlFor="email">Email</label>
-        <Field name="email"></Field>
-        <label htmlFor="password">Email</label>
-        <Field name="password"></Field>
+        <label htmlFor={nameFieldId}>Username</label>
+        <Field name="name" id={nameFieldId}></Field>
+        <label htmlFor={emailFieldId}>Email</label>
+        <Field name="email" id={emailFieldId}></Field>
+        <label htmlFor={passwordFieldId}>Password</label>
+        <Field name="password" id={passwordFieldId} type="password"></Field>
+        <button type="submit">Register</button>
       </Form>
     </Formik>
   );
